d59ffc5863bf40f16648304ac207f99b856fc80a
Edios44 <161240985+Edios44@users.noreply.github.com>
2025-08-17 11:12:54 +0100

    added key to mapped componenets

--- src/output.jsx
+++ src/output.jsx
@@ -9,23 +9,23 @@ function GeneralInformation({ fullName, email, phone }) {
 }
 function CVContent({ education, experience }) {
   const educationItems = education.map((edu) => (
-    <>
+    <div key={edu.id}>
       <h2>{edu.degree}</h2>
       <div>at {edu.school}</div>
       <div>
         from : {edu.educationStart} to: {edu.educationEnd}
       </div>
-    </>
+    </div>
   ));
   const experienceItems = experience.map((exp) => (
-    <>
+    <div key={exp.id}>
       <h2>{exp.position}</h2>
       <div>at {exp.company}</div>
       <p>{exp.responsibilities}</p>
       <div>
         from : {exp.experienceStart} to: {exp.experienceEnd}
       </div>
-    </>
+    </div>
   ));
   return (
     <>
