0733d9d3fbf7c783ea18d729e48e410f2de9c1cc
Edios44 <161240985+Edios44@users.noreply.github.com>
2025-08-18 11:18:22 +0100

    style

--- src/input.jsx
+++ src/input.jsx
@@ -8,37 +8,47 @@ function GeneralInformationForm({
   name,
   email,
   phone,
+  style,
 }) {
   return (
     <>
       <form>
-        <h1 className="font-extrabold text-4xl">General Information</h1>
-        <ul>
-          <li>
-            <label htmlFor="fullName">Full Name</label>
+        <h1 className={style.classH1}>General Information</h1>
+        <ul className={style.classUL}>
+          <li className={style.classLi}>
+            <label htmlFor="fullName" className={style.classLabel}>
+              Full Name
+            </label>
             <input
               type="text"
               id="fullName"
               onChange={(e) => setFullName(e.target.value)}
               defaultValue={name}
+              className={style.classInput}
             />
           </li>
-          <li>
-            <label htmlFor="email">Email</label>
+          <li className={style.classLi}>
+            <label htmlFor="email" className={style.classLabel}>
+              Email
+            </label>
             <input
               type="email"
               id="email"
               onChange={(e) => setEmail(e.target.value)}
               defaultValue={email}
+              className={style.classInput}
             />
           </li>
-          <li>
-            <label htmlFor="phone">Phone</label>
+          <li className={style.classLi}>
+            <label htmlFor="phone" className={style.classLabel}>
+              Phone
+            </label>
             <input
               type="tel"
               id="phone"
               onChange={(e) => setPhone(e.target.value)}
               defaultValue={phone}
+              className={style.classInput}
             />
           </li>
         </ul>
@@ -46,62 +56,88 @@ function GeneralInformationForm({
     </>
   );
 }
-function EducationForm({ educationHandler, showForm, id, setShowForm, edu }) {
+function EducationForm({
+  educationHandler,
+  showForm,
+  id,
+  setShowForm,
+  edu,
+  style,
+}) {
   return (
     <>
       <form onSubmit={(e) => educationHandler(e, setShowForm)} id={id}>
-        <h1>Education</h1>
-        <ul>
-          <li>
-            <label htmlFor="school">School</label>
+        <h1 className={style.classH1}>Education</h1>
+        <ul className={style.classUL}>
+          <li className={style.classLi}>
+            <label htmlFor="school" className={style.classLabel}>
+              School
+            </label>
             <input
               type="text"
               id="school"
               defaultValue={edu !== undefined ? edu.school : null}
+              className={style.classInput}
             />
           </li>
-          <li>
-            <label htmlFor="degree">Degree</label>
+          <li className={style.classLi}>
+            <label htmlFor="degree" className={style.classLabel}>
+              Degree
+            </label>
             <input
               type="text"
               id="degree"
               defaultValue={edu !== undefined ? edu.degree : null}
+              className={style.classInput}
             />
           </li>
-          <li>
-            <label htmlFor="educationStart">Start Date</label>
+          <li className={style.classLi}>
+            <label htmlFor="educationStart" className={style.classLabel}>
+              Start Date
+            </label>
             <input
               type="date"
               id="educationStart"
               defaultValue={edu !== undefined ? edu.educationStart : null}
+              className={style.classInput}
             />
-            <label htmlFor="educationEnd">End Date</label>
+            <label htmlFor="educationEnd" className={style.classLabel}>
+              End Date
+            </label>
             <input
               type="date"
               id="educationEnd"
               defaultValue={edu !== undefined ? edu.educationEnd : null}
+              className={style.classInput}
             />
           </li>
         </ul>
-        <button type="submit">{showForm == null ? "Add" : "Edit"}</button>
+        <button type="submit" className={style.classBtn}>
+          {showForm == null ? "Add" : "Edit"}
+        </button>
       </form>
     </>
   );
 }
-function EducationPreview({ education, editEducation }) {
+function EducationPreview({ education, editEducation, style, style2nd }) {
   const [showForm, setShowForm] = useState(null);
   const educationItems = education.map((edu) => (
-    <div key={edu.id}>
-      <h2>{edu.degree}</h2>
-      <div>at {edu.school}</div>
-      <div>
-        from : {edu.educationStart} to: {edu.educationEnd}
+    <div key={edu.id} className={style.classContainerSecondary}>
+      <h2 className={style.classH3}>{edu.degree}</h2>
+      <div className="">
+        <i>at {edu.school}</i>
+      </div>
+      <div className="text-sm text-gray-500">
+        <i>
+          from : {edu.educationStart} to: {edu.educationEnd}
+        </i>
       </div>
       <button
         id={edu.id}
         onClick={() =>
           showForm != edu.id ? setShowForm(edu.id) : setShowForm(null)
         }
+        className={style.classBtn}
       >
         Edit
       </button>
@@ -112,82 +148,110 @@ function EducationPreview({ education, editEducation }) {
           id={edu.id}
           setShowForm={setShowForm}
           edu={edu}
+          style={style2nd}
         />
       )}
     </div>
   ));
   return (
-    <>
-      <h2>Education:</h2>
+    <div className="my-3">
+      <h2 className={style.classH2}>Your Education Degrees:</h2>
       {educationItems}
-    </>
+    </div>
   );
 }
-function ExperienceForm({ experienceHandler, showForm, id, setShowForm, exp }) {
+function ExperienceForm({
+  experienceHandler,
+  showForm,
+  id,
+  setShowForm,
+  exp,
+  style,
+}) {
   return (
     <>
       <form onSubmit={(e) => experienceHandler(e, setShowForm)} id={id}>
-        <h1>Experience</h1>
-        <ul>
-          <li>
-            <label htmlFor="company">Company</label>
+        <h1 className={style.classH1}>Experience</h1>
+        <ul className={style.classUL}>
+          <li className={style.classLi}>
+            <label htmlFor="company" className={style.classLabel}>
+              Company
+            </label>
             <input
               type="text"
               id="company"
               defaultValue={exp !== undefined ? exp.company : null}
+              className={style.classInput}
             />
           </li>
-          <li>
-            <label htmlFor="position">Position</label>
+          <li className={style.classLi}>
+            <label htmlFor="position" className={style.classLabel}>
+              Position
+            </label>
             <input
               type="text"
               id="position"
               defaultValue={exp !== undefined ? exp.position : null}
+              className={style.classInput}
             />
           </li>
-          <li>
-            <label htmlFor="responsibilities">Responsibilities</label>
+          <li className={style.classLi}>
+            <label htmlFor="responsibilities" className={style.classLabel}>
+              Responsibilities
+            </label>
             <input
               type="text"
               id="responsibilities"
               defaultValue={exp !== undefined ? exp.responsibilities : null}
+              className={style.classInput}
             />
           </li>
-          <li>
+          <li className={style.classLi}>
             <label htmlFor="experienceStart">Start Date</label>
             <input
               type="date"
               id="experienceStart"
               defaultValue={exp !== undefined ? exp.experienceStart : null}
+              className={style.classInput}
             />
-            <label htmlFor="experienceEnd">End Date</label>
+            <label htmlFor="experienceEnd" className={style.classLabel}>
+              End Date
+            </label>
             <input
               type="date"
               id="experienceEnd"
               defaultValue={exp !== undefined ? exp.experienceEnd : null}
+              className={style.classInput}
             />
           </li>
         </ul>
-        <button type="submit">{showForm == null ? "Add" : "Edit"}</button>
+        <button type="submit" className={style.classBtn}>
+          {showForm == null ? "Add" : "Edit"}
+        </button>
       </form>
     </>
   );
 }
-function ExperiencePreview({ experience, editExperience }) {
+function ExperiencePreview({ experience, editExperience, style, style2nd }) {
   const [showForm, setShowForm] = useState(null);
   const experienceItems = experience.map((exp) => (
-    <div key={exp.id}>
-      <h2>{exp.position}</h2>
-      <div>at {exp.company}</div>
-      <p>{exp.responsibilities}</p>
+    <div key={exp.id} className={style.classContainerSecondary}>
+      <h3 className={style.classH3}>{exp.position}</h3>
       <div>
-        from : {exp.experienceStart} to: {exp.experienceEnd}
+        <i>at {exp.company}</i>
+      </div>
+      <p>{exp.responsibilities}</p>
+      <div className="text-sm text-gray-500">
+        <i>
+          from : {exp.experienceStart} to: {exp.experienceEnd}
+        </i>
       </div>
       <button
         id={exp.id}
         onClick={() =>
           showForm != exp.id ? setShowForm(exp.id) : setShowForm(null)
         }
+        className={style.classBtn}
       >
         Edit
       </button>
@@ -198,15 +262,16 @@ function ExperiencePreview({ experience, editExperience }) {
           id={exp.id}
           setShowForm={setShowForm}
           exp={exp}
+          style={style2nd}
         />
       )}
     </div>
   ));
   return (
-    <>
-      <h2>Experience:</h2>
+    <div className="my-3">
+      <h2 className={style.classH2}>Your Work Experience:</h2>
       {experienceItems}
-    </>
+    </div>
   );
 }
 
@@ -224,8 +289,38 @@ export default function Input({
   addExperience,
   editExperience,
 }) {
+  const classContainerPrimary =
+    "m-4 border-1 border-gray-400 rounded-md p-3 shadow";
+  const classContainerSecondary =
+    "m-2 border-2 border-gray-600 rounded-md p-2 shadow";
+  const classH1 = "font-extrabold text-5xl mb-3";
+  const classH2 = "font-bold text-3xl mb-3";
+  const classH3 = "font-medium text-2xl mb-3";
+  const classUL = "flex flex-col";
+  const classLi = "m-2";
+  const classLabel = "font-medium mx-2 w-30 inline-block";
+  const classInput =
+    "focus:border-gray-500 focus:border-2 rounded-md p-1 outline-none border-2 border-transparent";
+  const classBtn =
+    "border-1 text-sm p-1.5 rounded-md cursor-pointer shadow mt-4";
+  const style_input = {
+    classContainerPrimary,
+    classH1,
+    classUL,
+    classLi,
+    classLabel,
+    classInput,
+    classBtn,
+  };
+  const style_preview = {
+    classContainerPrimary,
+    classContainerSecondary,
+    classH2,
+    classH3,
+    classBtn,
+  };
   return (
-    <>
+    <div className="m-8 max-w-2xl">
       <GeneralInformationForm
         setFullName={setFullName}
         setEmail={setEmail}
@@ -233,14 +328,26 @@ export default function Input({
         name={name}
         email={email}
         phone={phone}
+        style={style_input}
       />
-      <EducationForm educationHandler={addEducation} />
-      <EducationPreview education={education} editEducation={editEducation} />
-      <ExperienceForm experienceHandler={addExperience} />
-      <ExperiencePreview
-        experience={experience}
-        editExperience={editExperience}
-      />
-    </>
+      <div className="my-8">
+        <EducationForm educationHandler={addEducation} style={style_input} />
+        <EducationPreview
+          education={education}
+          editEducation={editEducation}
+          style={style_preview}
+          style2nd={style_input}
+        />
+      </div>
+      <div className="my-8">
+        <ExperienceForm experienceHandler={addExperience} style={style_input} />
+        <ExperiencePreview
+          experience={experience}
+          editExperience={editExperience}
+          style={style_preview}
+          style2nd={style_input}
+        />
+      </div>
+    </div>
   );
 }
